93d1661a4dc6fa4c084a194590b88b613a47c0c1
P Bridwell <patbridwell@cox.net>
2020-04-19 16:13:49 -0500

    adds routes for home page and notes page

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-// require("./routes/apiRoutes")(app);
+require("./public/routes/htmlroutes")(app);
 // require("./routes/htmlRoutes")(app);
 
 // =============================================================================
